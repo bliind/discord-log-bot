66589012453bd5ae32d51c41e0ed6977c3acb3f3
Mike Sweeney <blindx@gmail.com>
2026-07-29 13:28:57 -0400

    properly fix missing config

--- Cogs/VoiceMakerCog.py
+++ Cogs/VoiceMakerCog.py
@@ -23,8 +23,9 @@ class VoiceMakerCog(commands.Cog):
         vc_creation_channel = self.bot.config[member.guild.id].vc_creation_channel
         vc_creation_category = self.bot.config[member.guild.id].vc_creation_category
 
-        if not created_voice_channels:
-            created_voice_channels = []
+        if created_voice_channels == None:
+            self.bot.config[member.guild.id].created_voice_channels = []
+            created_voice_channels = self.bot.config[member.guild.id].created_voice_channels
 
         if after.channel and after.channel.id == vc_creation_channel:
             if vc_creation_category:
